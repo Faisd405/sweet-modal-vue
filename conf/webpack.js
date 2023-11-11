53941bf78008c730ee9690824454716bf1d7cfee
faisd405 <faisd405@gmail.com>
2023-11-11 18:33:32 +0700

    refactor: update dependency package.json, and setup webpack

--- conf/webpack.js
+++ conf/webpack.js
@@ -1,21 +1,16 @@
 var path = require('path')
 var webpack = require('webpack')
-var MinifyPlugin = require('babel-minify-webpack-plugin')
 
 // Determine which env to use
 // by having it overriden at runtime using `cross-env NODE_ENV=...`
 // Possible envs: dev, production
-var node_env
-
-if (process.env.NODE_ENV) {
-	node_env = process.env.NODE_ENV.toLowerCase()
-} else {
-	node_env = 'production'
-}
+var node_env = process.env.NODE_ENV ? process.env.NODE_ENV.toLowerCase() : 'production';
 
 console.log('ENV', node_env)
 
 module.exports = {
+	mode: node_env === 'production' ? 'production' : 'development',
+
 	resolve: {
 		alias: {
 			'vue$': 'vue/dist/vue.js',
@@ -53,8 +48,7 @@ module.exports = {
 					options: {
 						presets: [['env', {
 							targets: {
-								browsers: ['last 2 versions', '>= 3%', 'not ie <= 10'],
-								uglify: true
+								browsers: ['last 2 versions', '>= 3%', 'not ie <= 10']
 							},
 							modules: false,
 							forceAllTransforms: node_env === 'production'
@@ -64,16 +58,20 @@ module.exports = {
 			},
 			{
 				test: /\.svg$/,
-				use: 'svg-inline-loader?classPrefix'
+				use: [
+				  {
+					loader: 'svg-inline-loader',
+					options: {
+					  classPrefix: true
+					}
+				  }
+				]
 			},
 			{
 				test: /\.(png|jpg|gif|svg)$/,
 				use: [
 					{
 						loader: 'file-loader',
-						query: {
-							name: '[name].[ext]?[hash]'
-						}
 					}
 				]
 			}
@@ -82,26 +80,13 @@ module.exports = {
 
 	plugins: [
 		new webpack.DefinePlugin({
-			'process.env': {
-				NODE_ENV: '"' + node_env + '"'
-			}
+			'process.env.NODE_ENV': JSON.stringify(node_env)
 		})
 	],
-
-	devtool: '#cheap-source-map',
 
 	devServer: {
 		historyApiFallback: true,
 		noInfo: true,
 		port: 8081
 	}
-}
-
-if (node_env == 'production') {
-	module.exports.devtool = '#source-map'
-	module.exports.plugins = module.exports.plugins.concat([
-		new MinifyPlugin({}, {
-			comments: false
-		})
-	])
 }
